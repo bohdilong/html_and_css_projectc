25e2b44dd921671f18eaac07a53f43bdd04185d2
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-02-02 13:42:18 -0800

    added Basic_HTML_and_JavaScript_1

---
+++ basic_html_and_javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello World"); 
+
+function My_First_Functiion() {
+    var String = "Kiss me, I'm Irish!";
+    var result = String.fontcolor("green");
+    document.getElementById("Irish").innerHTML = 
+    result;
+}
