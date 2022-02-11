1648de355df498109b92593bda321dd290d5f7ec
bohdilong <98560374+bohdilong@users.noreply.github.com>
2022-02-10 20:40:51 -0800

    col tag challenge index.html redo

--- basic_html_and_javascript_1/Basic_JavaScript_1.js
+++
@@ -1,8 +0,0 @@
-alert("Hello World"); 
-
-function My_First_Functiion() {
-    var String = "Kiss me, I'm Irish!";
-    var result = String.fontcolor("green");
-    document.getElementById("Irish").innerHTML = 
-    result;
-}
